f8026cc8e3e6e4ecfa31c63beee5d6d414ed231b
usenkin83 <usenkin83@gmail.com>
2024-12-03 22:11:52 +0200

    full

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:1
+        }
+    }
+
+})
